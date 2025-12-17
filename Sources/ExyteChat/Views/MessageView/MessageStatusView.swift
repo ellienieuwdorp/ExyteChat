1822b9735f031561cb4a6a96ce9711e22a354bed
lissine <lissine@ellini.org>
2025-12-17 22:28:40 +0100

    Revert "Message.Status delivered status code cleanup"
    This reverts commit baaea88c620c0850cde22f724a97dd1164f739d9.

--- Sources/ExyteChat/Views/MessageView/MessageStatusView.swift
+++ Sources/ExyteChat/Views/MessageView/MessageStatusView.swift
@@ -14,32 +14,42 @@ struct MessageStatusView: View {
     var body: some View {
         Group {
             switch status {
-            case .sending:
-                statusImageStyled(image: theme.images.message.sending, color: getTheme().colors.statusGray)
-            case .sent:
-                statusImageStyled(image: theme.images.message.sent, color: getTheme().colors.statusGray)
-            case .delivered:
-                statusImageStyled(image: theme.images.message.delivered, color: getTheme().colors.statusGray)
+            case .sending, .sent, .delivered:
+              getImagge(status)
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(getTheme().colors.statusGray)
+                    .frame(width: 40)
             case .read:
-                statusImageStyled(image: theme.images.message.read, color: getTheme().colors.messageReadStatus)
+              getImagge(status)
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(getTheme().colors.messageReadStatus)
+                    .frame(width: 40)
             case .error:
                 Button(action: onRetry) {
-                    statusImageStyled(image: theme.images.message.error, color: getTheme().colors.statusError)
+                    getTheme().images.message.error
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40)
                 }
+                .foregroundColor(theme.colors.statusError)
             }
         }
         .viewSize(MessageView.statusViewSize)
         .padding(.trailing, MessageView.horizontalStatusPadding)
     }
 
-    private func statusImageStyled(image: Image, color: Color) -> some View {
-        return
-            image
-            .renderingMode(.template)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .foregroundColor(color)
-            .frame(width: 40)
+    private func getImagge(_ status: Message.Status) -> Image {
+        switch status {
+        case .sending: return theme.images.message.sending
+        case .sent: return theme.images.message.sent
+        case .delivered: return theme.images.message.delivered
+        case .read: return theme.images.message.read
+        case .error: return theme.images.message.error
+        }
     }
 
     @MainActor
@@ -49,26 +59,13 @@ struct MessageStatusView: View {
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
-    
     static var previews: some View {
         VStack {
             MessageStatusView(status: .sending, onRetry: {})
             MessageStatusView(status: .sent, onRetry: {})
             MessageStatusView(status: .delivered, onRetry: {})
             MessageStatusView(status: .read, onRetry: {})
-            MessageStatusView(status: .error(emptyDraft()), onRetry: {})
         }
     }
-
-    private static func emptyDraft() -> DraftMessage {
-        return DraftMessage(
-            id: nil,
-            text: "",
-            medias: [],
-            recording: nil,
-            replyMessage: nil,
-            createdAt: Date()
-        )
-
-    }
 }
+
